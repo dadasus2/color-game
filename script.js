3bce7d6a4498b09a60b819860864b99ab8829ebf
Daniel <bohatec.daniel@stud.vassboskovice.cz>
2023-06-14 23:04:43 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getRandomColor() {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -7,38 +8,18 @@ function getRandomColor() {
     return color;
 }
 
-function createSquares() {
+let baseHue = Math.floor(Math.random() * 500);
+let saturation = Math.floor(Math.random() * 11) + 45;
+let lightness = Math.floor(Math.random() * 11) + 45;
+function getSimilarColor() {
 
-    const color = getRandomColor();
+    return `hsl(${baseHue}, ${saturation}%, ${lightness}%)`;
+}
 
-    const container = document.querySelector(".container");
-    const uniqueSquare = document.createElement("div");
-    const randomOrder = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-    uniqueSquare.classList.add("square", "unique");
-    uniqueSquare.style.backgroundColor = getRandomColor();
-    uniqueSquare.style.order = randomOrder
-    container.appendChild(uniqueSquare);
+function getSimilarUniqueColor() {
+    baseHue += Math.floor(Math.random() * 100)
 
-
-
-    for (let i = 0; i < 8; i++) {
-
-        var square = document.createElement("div");
-        square.classList.add("square");
-        square.style.backgroundColor = color;
-        square.style.order = Math.floor(Math.random() * 8) + 1
-        container.appendChild(square);
-        square.addEventListener('click', () => {
-            clickNormal()
-        })
-    }
-
-    const unique = document.querySelector('.unique')
-
-    unique.addEventListener('click', () => {
-        clickUnique()
-    })
-
+    return `hsl(${baseHue}, ${saturation}%, ${lightness}%)`;
 }
 
 function clickUnique() {
@@ -70,6 +51,60 @@ function updateScore() {
     var score = document.querySelector('.counter')
     score.textContent = scoreCount
 }
+
+
+
+function createSquares() {
+
+    const color = getSimilarColor();
+
+    const container = document.querySelector(".container");
+    const uniqueSquare = document.createElement("div");
+    const randomOrder = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    uniqueSquare.classList.add("square", "unique");
+    uniqueSquare.style.backgroundColor = getSimilarUniqueColor();
+    uniqueSquare.style.order = randomOrder
+    container.appendChild(uniqueSquare);
+
+
+
+    for (let i = 0; i < 8; i++) {
+
+        var square = document.createElement("div");
+        square.classList.add("square");
+        square.style.backgroundColor = color;
+        square.style.order = Math.floor(Math.random() * 8) + 1
+        container.appendChild(square);
+        square.addEventListener('click', () => {
+            clickNormal()
+        })
+    }
+
+
+    const unique = document.querySelector('.unique')
+
+    unique.addEventListener('click', () => {
+        clickUnique()
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
